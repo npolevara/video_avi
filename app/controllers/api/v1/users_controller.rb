50f75b2ac5dce86190e838c08ede57e1e9ddec36
Nik <npolevara@gmail.com>
2019-11-17 22:57:25 +0200

    [controller] add logic to users#signin

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def signin
-
+    user = User.where(name: params[:name], key: params[:key] ).first
+    render json: user.as_json(only:[:name, :key]), status: :accepted if user
+    render json: User.create!(name: params[:name], key: params[:key]).as_json(only:[:name, :key]), status: :created if !user
   end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -2,15 +2,32 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::UsersController', type: :request do
   describe 'POST Signin' do
+    json = { 'CONTENT_TYPE' => 'application/json' }
+    let!(:user) { User.create(name:'Max', key: '12345678') }
+
     context 'new user' do
       it 'should return new auth key' do
+        params = { 'name' => 'David', key: '00000' }.to_json
 
+        post '/api/v1/signin', headers: json, params: params
+        body = JSON.parse(response.body)
+        expect(response.status).to eq 201 # created
+        expect(body.has_key?('name')).to eq true
+        expect(body['name']).to eq 'David'
+        expect(body['key']).to eq '00000'
       end
     end
 
     context 'signed user' do
       it 'should return user auth key' do
-        
+        params = { 'name' => user.name, key: user.key }.to_json
+
+        post '/api/v1/signin', headers: json, params: params
+        body = JSON.parse(response.body)
+        expect(response.status).to eq 202 # accepted
+        expect(body.has_key?('name')).to eq true
+        expect(body['name']).to eq user.name
+        expect(body['key']).to eq user.key
       end
     end
   end
