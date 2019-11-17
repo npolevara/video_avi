6b2e843c12c60030695a5ee08175a689f427eafe
Nik <npolevara@gmail.com>
2019-11-17 20:48:20 +0200

    [controller] mockup users controller

---
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::UsersController', type: :request do
+  describe 'POST Signin' do
+    context 'new user' do
+      it 'should return new auth key' do
+
+      end
+    end
+
+    context 'signed user' do
+      it 'should return user auth key' do
+        
+      end
+    end
+  end
+end
