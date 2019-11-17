7bc662f62c21dc7084960a7be6d236d7db7ad239
Nik <npolevara@gmail.com>
2019-11-17 23:29:39 +0200

    [controller] add stron params to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::UsersController < ApplicationController
+
   def signin
-    user = User.where(authentication_token: params[:key] ).first
+    user = User.where(required_params).first if required_params
     render json: user.as_json(only:[:authentication_token]), status: :accepted if user
     render json: User.create!.as_json(only:[:authentication_token]), status: :created if !user
   end
+
+  private
+
+  def required_params
+    token = params.permit(:_id, :authentication_token)
+    return if token.to_h.empty?
+    token
+  end
+
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -6,10 +6,20 @@ RSpec.describe 'Api::V1::UsersController', type: :request do
     let!(:user) { User.create }
 
     context 'new user' do
-      it 'should return new auth key' do
+      it 'should return new auth key if no params' do
         # params = { 'name' => 'David', key: '00000' }.to_json
 
         post '/api/v1/signin', headers: json
+        body = JSON.parse(response.body)
+        expect(response).to have_http_status(:created)
+        expect(body['authentication_token']).to eq User.last.authentication_token
+        expect(body['authentication_token']).to_not eq user.authentication_token
+      end
+
+      it 'should return new auth key wrong params' do
+        params = { 'name' => 'David', key: '00000' }.to_json
+
+        post '/api/v1/signin', headers: json, params: params
         body = JSON.parse(response.body)
         expect(response).to have_http_status(:created)
         expect(body['authentication_token']).to eq User.last.authentication_token
@@ -19,7 +29,7 @@ RSpec.describe 'Api::V1::UsersController', type: :request do
 
     context 'signed user' do
       it 'should return user auth key' do
-        params = { key: user.authentication_token }.to_json
+        params = { _id: user._id, authentication_token: user.authentication_token }.to_json
 
         post '/api/v1/signin', headers: json, params: params
         body = JSON.parse(response.body)
